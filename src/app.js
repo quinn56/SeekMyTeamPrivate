41b2637f2c6e29b7df52f8dd5ac5f97b3b89e6d2
brian <quinn56@purdue.edu>
2019-02-07 16:36:25 -0500

    add routes folder

--- src/app.js
+++ src/app.js
@@ -29,6 +29,9 @@ if (cluster.isMaster) {
     var express = require('express');
     var bodyParser = require('body-parser');
 
+    require('./routes')(app);
+
+
     AWS.config.region = process.env.REGION
 
     var sns = new AWS.SNS();
@@ -41,57 +44,6 @@ if (cluster.isMaster) {
     app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
     app.use(bodyParser.urlencoded({extended:false}));
-
-    app.get('/', function(req, res) {
-        res.render('index', {
-            static_path: 'static',
-            theme: process.env.THEME || 'flatly',
-            flask_debug: process.env.FLASK_DEBUG || 'false'
-        });
-    });
-
-    app.post('/signup', function(req, res) {
-        var item = {
-            'UserID' : {'S': '001'+userid},
-            'email': {'S': req.body.email},
-            'name': {'S': req.body.name},
-            'preview': {'S': req.body.previewAccess},
-            'theme': {'S': req.body.theme}
-        };
-        userid++;
-
-        ddb.putItem({
-            'TableName': ddbTable,
-            'Item': item,
-            'Expected': { email: { Exists: false } }
-        }, function(err, data) {
-            if (err) {
-                var returnStatus = 500;
-
-                if (err.code === 'ConditionalCheckFailedException') {
-                    returnStatus = 409;
-                }
-
-                res.status(returnStatus).end();
-                console.log('DDB Error: ' + err);
-            } else {
-                sns.publish({
-                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email
-                                        + "\r\nPreviewAccess: " + req.body.previewAccess
-                                        + "\r\nTheme: " + req.body.theme,
-                    'Subject': 'New user sign up!!!',
-                    'TopicArn': snsTopic
-                }, function(err, data) {
-                    if (err) {
-                        res.status(500).end();
-                        console.log('SNS Error: ' + err);
-                    } else {
-                        res.status(201).end();
-                    }
-                });
-            }
-        });
-    });
 
     var port = process.env.PORT || 3000;
 
