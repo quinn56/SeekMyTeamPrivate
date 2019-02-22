a86f4bc97daa20a1400ef3d992c2fe21bb0bbee3
Brian Quinn <quinn56@purdue.edu>
2019-02-22 00:32:53 -0500

    configure mailing from new domain

--- src/helpers/mailer.js
+++ src/helpers/mailer.js
@@ -2,7 +2,7 @@ const mg = require("../app").mg;
 
 function sendCode(email, code) {
     const data = {
-        from: 'postmaster@sandboxe1aea5edfa8f4904b65d4facc650f8c2.mailgun.org',
+        from: 'verify@mg.seekmyteam.com',
         to: email,
         subject: 'SeekMyTeam Verification Code',
         text: 'Hello, your verification code is: ' + code 
