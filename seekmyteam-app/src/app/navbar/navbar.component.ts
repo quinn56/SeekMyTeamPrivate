7bd2038a2f747416f406a6a0eaca744c869eb1d0
shengqiw <shengqiw97@gmail.com>
2019-03-27 20:57:14 -0400

    add css styles to app.module.html

--- seekmyteam-app/src/app/navbar/navbar.component.ts
+++ seekmyteam-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { AuthenticationService } from '../services/authentication/authentication.service';
 import { UserUtilsService } from '../services/users/user-utils.service';
 import { Router } from '@angular/router';
@@ -8,10 +8,19 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnChanges {
+  profilePic: string;
   isCollapsed: Boolean = true;
   currentEmail: string;
   constructor(private auth: AuthenticationService, private user_utils: UserUtilsService, private router: Router) { }
+
+  ngOnInit() {
+    this.getProfilePic();
+  }
+
+  ngOnChanges() {
+    this.getProfilePic();
+  }
 
   routeProfile() {
     this.currentEmail = this.user_utils.getCurrentUserDetails().email;
@@ -20,5 +29,13 @@ export class NavbarComponent {
 
   logout() {
     this.auth.logout();
+  }
+
+  getProfilePic() {
+    this.user_utils.getProfilePic(this.user_utils.getCurrentUserDetails().email).subscribe(pic => {
+      this.profilePic = pic.image;
+    }, (err) => {
+      console.log(err);
+    });
   }
 }
