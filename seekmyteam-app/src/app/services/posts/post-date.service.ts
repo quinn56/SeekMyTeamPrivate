7826b7f5ba833fcf5dc23f689a034bec2572efbc
Shengqi <shengqiw97@gmail.com>
2019-06-06 21:41:13 -0400

    hours hours hours pt2 :p

--- seekmyteam-app/src/app/services/posts/post-date.service.ts
+++ seekmyteam-app/src/app/services/posts/post-date.service.ts
@@ -5,14 +5,23 @@ export class PostDateService {
     constructor() {
     }
 
-    buildDate(date:number): string {
+    buildDate(date: number): string {
         // // TODO: Build string here that tells how old 'date' is from now
         // var today = new Date();
         // var hours: number = Math.abs(today - date) / 36e5;
         let now = Date.now();
         let elapsed = now - date;
-        elapsed = (elapsed / (1000*60*60)) % 24;
-        var hourstr: string = "Posted " + elapsed + "hrs ago"
-        return hourstr;
+        elapsed = (elapsed / (1000 * 60 * 60)) % 24;
+        let hourstr: string;
+
+        if (elapsed < 1)
+            return "Posted less 1 hr ago";
+        else if (elapsed < 24) {
+            return "Posted " + Math.round(elapsed) + " hrs ago";
+        }
+        else {
+            let days = Math.round(elapsed / 24);
+            return "Posted " + days + " day(s) ago";
+        }
     }
 }
