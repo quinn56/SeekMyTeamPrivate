eb9530e945540dd35e9cee74ccc91833413e43d0
David Wood <wood154@purdue.edu>
2019-06-26 17:22:28 -0500

    bug fixes to register modal

--- seekmyteam-app/src/app/app.module.ts
+++ seekmyteam-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -74,7 +74,8 @@ const appRoutes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
     NgbModule.forRoot(),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     AuthGuardService,
--- seekmyteam-app/src/app/register/register.component.ts
+++ seekmyteam-app/src/app/register/register.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { AuthenticationService, RegisterPayload, ConfirmPayload } from '../services/authentication/authentication.service';
 import { Router } from '@angular/router';
 import { AuthGuardService } from '../services/authentication/auth-guard.service';
 import { UserUtilsService } from '../services/users/user-utils.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+// @NgModule({
+//     imports: [
+//         FormBuilder,
+//         FormGroup,
+//         Validators
+//     ]
+// })
+
+class MoreInfo {
+    description: any;
+    skills: any[];
+    image: Object;
+}
 
 @Component({
     templateUrl: './register.component.html'
@@ -22,24 +37,47 @@ export class RegisterComponent {
         code: ''
     };
 
-    moreInfo: Object = {
+    moreInfo: MoreInfo = {
         description: '',
-        
+        skills: [],
+        image: null
     }
+
+    SKILLS_ARRAY: string[] = [
+        'Web Development',
+        'Backend Development',
+        'Full Stack Development',
+        'Project Management',
+        'Database Management'
+    ];
 
     notRegistered: boolean = false;
     serverError: boolean = false;
     invalidCode: boolean = false;
+    validCode: boolean = false;
+
+    nameForm: FormControl;
+    emailForm: FormControl;
+    passwordForm: FormControl;
+    schoolForm: FormControl;
+    majorForm: FormControl;
+
+    submitted = false;
 
     constructor(private auth: AuthenticationService,
         private auth_guard: AuthGuardService,
         private user_utils: UserUtilsService,
         private router: Router,
-        private route: ActivatedRoute) { }
+        private route: ActivatedRoute,
+        private formBuilder: FormBuilder) { }
 
-    ngOnInit() {
-        
-    }
+    ngOnInit() { 
+        this.nameForm = new FormControl(['', Validators.required]);
+        this.emailForm = new FormControl(['', Validators.required], this.validateEmail);
+        this.passwordForm = new FormControl(['', Validators.required]);
+        this.schoolForm = new FormControl(['', Validators.required]);
+        this.majorForm = new FormControl(['', Validators.required]);
+     }
 
     initConfirm() {
         this.confirmCredentials.email = this.credentials.email;
@@ -52,11 +90,9 @@ export class RegisterComponent {
     }
     
     register() {
-        if (!this.validateEmail(this.credentials.email)) {
-            console.log("invalid purdue email address");
-        } else {
+        this.submitted = true;
+        console.log("submitted");
             this.auth.register(this.credentials).subscribe((data) => { 
-                //this.router.navigateByUrl('/confirm/' + this.credentials.email);
                 this.initConfirm();
             }, (err) => {
                 if (err.status == 401) {
@@ -65,26 +101,44 @@ export class RegisterComponent {
                     console.log('server error: failed to register');
                 }
             });
-        }
+            
+        
+        
     }
 
-    validateEmail(email: string): boolean {
+    validateEmail(eml: FormControl) {
+        let email = eml.value;
         var re = /\S+@\S+\.\S+/;
         let val = re.test(email);
         if (!val) {
-            return false;
+            return email;
         } else {
-            if (email.substr(email.indexOf('@')) !== '@purdue.edu') {
-                return false;
+            if (email.indexOf('@') === -1) {
+                return {
+                    emailDomain: {
+                        parsedDomain: email.substr(email.indexOf('@'))
+                      }
+                };
             }
-            return true;
+            else if (email.substr(email.indexOf('@')) !== '@purdue.edu') {
+                console.log("returned NOT null");
+                return {
+                    emailDomain: {
+                        parsedDomain: email.substr(email.indexOf('@'))
+                      }
+                };
+            }
+            console.log("returned null")
+            return null;
         }
     }
 
     confirm() {
         console.log(this.confirmCredentials);
         this.auth.confirm(this.confirmCredentials).subscribe(() => {
-            this.router.navigateByUrl('/profile/' + this.confirmCredentials.email);
+            //this.router.navigateByUrl('/profile/' + this.confirmCredentials.email);
+            this.validCode = true;
+            this.invalidCode = false;
         }, (err) => {
             if (err.status == 401) {
                 console.log('you have not registered yet');
@@ -103,7 +157,7 @@ export class RegisterComponent {
         this.router.navigateByUrl('/profile/' + this.confirmCredentials.email);
     }
 
-    submitMoreInfo(info: Object) {
+    submitMoreInfo() {
         this.router.navigateByUrl('/profile/' + this.confirmCredentials.email);
         
     }
