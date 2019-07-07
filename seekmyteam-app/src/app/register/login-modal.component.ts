e848dc537ada5236a6ce9ddcb8d7de035d3bdfdc
David Wood <wood154@purdue.edu>
2019-07-07 11:10:21 -0500

    coming-soon page and restructuring login/register modals

---
+++ seekmyteam-app/src/app/register/login-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginPayload, AuthenticationService } from '../services/authentication/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+})
+export class LoginModalComponent implements OnInit {
+  invalidLoginEmail: boolean = false;
+  invalidLoginPassword: boolean = false;
+  unconfirmedEmail: boolean = false;
+  
+  loginCredentials: LoginPayload = {
+    email: '',
+    password: ''
+  };
+  constructor(private auth: AuthenticationService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.invalidLoginEmail = false;
+    this.invalidLoginPassword = false;
+    this.auth.login(this.loginCredentials).subscribe(() => {
+        location.reload();
+        this.router.navigateByUrl('');
+    }, (err) => {
+        if (err.status == 400) {
+            this.invalidLoginEmail = true;
+        } else if (err.status == 401) {
+            this.unconfirmedEmail = true;
+        } else if (err.status == 402) {
+            this.invalidLoginPassword = true;
+        } else {
+            console.log('Server error: Could not login')
+        }
+    });
+  }
+}
