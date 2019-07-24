31ca263f12e36cdad440bf454de2ebf6223a4228
Shengqi <shengqiw97@gmail.com>
2019-07-23 22:11:17 -0400

    added new post modal

--- seekmyteam-app/src/app/navbar/navbar.component.ts
+++ seekmyteam-app/src/app/navbar/navbar.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnChanges {
   profilePic: string;
